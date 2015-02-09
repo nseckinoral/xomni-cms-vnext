557ba867ef4f1b5f23c3eb39034482b3282453eb
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-02-09 10:19:47 +0200

    Analytics summary test codes implemented.

--- src/XOMNI.CMS.FrontEnd/src/pages/private/analytics-summary-page/analytics-summary.ts
+++ src/XOMNI.CMS.FrontEnd/src/pages/private/analytics-summary-page/analytics-summary.ts
@@ -7,4 +7,24 @@ export class viewModel {
     constructor(params: any) {
 
     }
+
+    public list(skip: number = 0, take: number = 25): void {
+        this.getBrands(skip, take, r => {
+            this.pagedBrands(r.Results);
+            this.totalCount(r.TotalCount);
+        });
+    }
+
+    public getBrands(skip: number, take: number, callback: (result: any) => void): any {
+        jquery.ajax("http://test.apistaging.xomni.com/private/catalog/brands?skip=" + skip + "&take=" + take, {
+            type: "get",
+            contentType: "application/json",
+            success: (d, t, s) => {
+                callback(d);
+            },
+            error: (r, t, e) => {
+                alert(t);
+            }
+        });
+    }
 }
