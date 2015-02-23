affbf8bb618861654456d0e853c497f27e07fc04
Gokhan Gulbiz <ggulbiz@gmail.com>
2015-02-23 18:24:20 +0200

    Test changes rollbacked.

--- src/XOMNI.CMS.FrontEnd/src/pages/private/analytics-summary-page/analytics-summary.ts
+++ src/XOMNI.CMS.FrontEnd/src/pages/private/analytics-summary-page/analytics-summary.ts
@@ -15,18 +15,10 @@ export class viewModel {
     public selectedClientCounters = ko.observableArray([]);
 
     constructor() {
-        //this.showLoadingDialog();
+        this.showLoadingDialog();
         this.initializeSlider();
-        //this.initializeSDK();
-        //this.loadClientCounters();
-        new Chartist.Line('.ct-chart', {
-            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-            series: [
-                [12, 9, 7, 8, 5],
-                [2, 1, 3.5, 7, 3],
-                [1, 3, 4, 5, 6]
-            ]
-        });
+        this.initializeSDK();
+        this.loadClientCounters();
     }
 
     initializeSlider() {
