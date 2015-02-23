2eccf5c4e205e65806d206fb2169222af4f9bbde
ilkayilknur <ilkay@ilkayilknur.com>
2015-02-23 15:27:50 +0200

    Master API SDK added and licence cap API implemented.

---
+++ src/XOMNI.CMS.BackEnd/Managers/LicenceManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using XOMNI.SDK.Master;
+
+namespace XOMNI.CMS.BackEnd.Managers
+{
+    public class LicenceManager : ILicenceManager
+    {
+        private MasterAPIClientContext MasterAPIClientContext;
+        public LicenceManager(MasterAPIClientContext masterAPIClientContext)
+        {
+            this.MasterAPIClientContext = masterAPIClientContext;
+        }
+
+        public Task<int> GetAsync(string tenantName)
+        {
+            var licenceClient = MasterAPIClientContext.GetLicenceManagementClient();
+            return licenceClient.GetLicenceCapAsync(tenantName);
+        }
+    }
+}
