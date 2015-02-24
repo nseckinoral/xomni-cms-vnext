390e2ca6ccb73d2037594aea107227066cb5a365
Daron Yondem <daron@yondem.com>
2015-02-24 18:21:35 +0200

    Unnecessary Comments Removed.

--- src/XOMNI.CMS.FrontEnd/src/components/side-bar/side-bar.ts
+++ src/XOMNI.CMS.FrontEnd/src/components/side-bar/side-bar.ts
@@ -8,9 +8,6 @@ export class viewModel {
     public navigationItems = ko.observableArray([]);
 
     constructor(params: any) {
-        // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
-        // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
-        // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
 
         this.route = params.route;
         this.fethNavigationData(2);
