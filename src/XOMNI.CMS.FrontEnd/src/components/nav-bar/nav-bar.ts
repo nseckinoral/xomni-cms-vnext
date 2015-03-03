f537a0bb3bd1029c4e9d219811ff03a88c0860c8
ilkayilknur <ilkay@ilkayilknur.com>
2015-03-03 17:37:17 +0200

    Fixes #10 . upper menu links updated.

--- src/XOMNI.CMS.FrontEnd/src/components/nav-bar/nav-bar.ts
+++ src/XOMNI.CMS.FrontEnd/src/components/nav-bar/nav-bar.ts
@@ -7,6 +7,11 @@ export class viewModel extends cms.infrastructure.baseViewModel {
     private shouter: any;
     public route: any;
     private managementEnabled = ko.observable(false);
+    private oldCMSUrl = this.getOldCMSUrl();
+    private managementLink = ko.observable(this.oldCMSUrl + '/Stores/List.aspx');
+    private catalogLink = ko.observable(this.oldCMSUrl + '/Brands/List.aspx');
+    private dashboardLink = ko.observable(this.oldCMSUrl + '/Dashboard.aspx');
+
     constructor(params: any) {
         super();
         this.route = params.route;
