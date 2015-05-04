5fe963f94540b599501a3f5a6f701966de1521ee
Sezgin Ege <mail@sezginege.com>
2015-05-04 14:42:08 +0300

    Implemented facebook-settings page.

--- src/XOMNI.CMS.FrontEnd/src/app/startup.ts
+++ src/XOMNI.CMS.FrontEnd/src/app/startup.ts
@@ -24,6 +24,7 @@ ko.components.register('management-integration-endpoint-page', { require: 'pages
 ko.components.register('management-msg-integration-page', { require: 'pages/management/msg-integration-page/msg-integration' });
 ko.components.register('management-tenant-settings-page', { require: 'pages/management/tenant-settings-page/tenant-settings' });
 ko.components.register('management-twitter-settings-page', { require: 'pages/management/settings-page/twitter-settings' });
+ko.components.register('management-facebook-settings-page', { require: 'pages/management/settings-page/facebook-settings' });
 //[[XO-SCAFFOLDER]]
 
 // Start the application
