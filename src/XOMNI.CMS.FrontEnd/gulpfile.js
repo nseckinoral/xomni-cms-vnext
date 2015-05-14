2baed9c3a71e6a6841dd8d9fb540f4dc692aaa81
ilkayilknur <ilkay@ilkayilknur.com>
2015-05-14 13:17:18 +0300

    Added a statement about bundling issue in gulp tasks.

--- src/XOMNI.CMS.FrontEnd/gulpfile.js
+++ src/XOMNI.CMS.FrontEnd/gulpfile.js
@@ -1,3 +1,4 @@
+//This gulp file is currently broken and not working. When we fix https://github.com/XomniCloud/xomni-cms-vnext/issues/16 issue we will fix the bundling problem. You can follow related issue(https://github.com/XomniCloud/xomni-cms-vnext/issues/37) for updates.
 // Node modules
 var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = require('chalk'), es = require('event-stream');
 
