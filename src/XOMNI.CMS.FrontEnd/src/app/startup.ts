8a235f08f3328317223834f18a09e936e8a058a9
Sezgin Ege <mail@sezginege.com>
2015-05-27 07:19:24 +0300

    Created facebook-settings screen files.

--- src/XOMNI.CMS.FrontEnd/src/app/startup.ts
+++ src/XOMNI.CMS.FrontEnd/src/app/startup.ts
@@ -24,6 +24,8 @@ ko.components.register('private-analytics-summary-page', { require: 'pages/priva
 ko.components.register('management-integration-endpoint-page', { require: 'pages/management/integration-endpoint-page/integration-endpoint' });
 ko.components.register('management-msg-integration-page', { require: 'pages/management/msg-integration-page/msg-integration' });
 ko.components.register('management-tenant-settings-page', { require: 'pages/management/tenant-settings-page/tenant-settings' });
+ko.components.register('management-facebook-settings-page', { require: 'pages/management/facebook-settings-page/facebook-settings' });
+
 //[[XO-SCAFFOLDER]]
 
 // Start the application
