1636e417955cb851db8a5da63610f0bc0db870f9
nseckinoral <nseckinoral@gmail.com>
2015-09-14 15:54:53 +0300

    Paginator- Removed unnecessary Math.floor in boundry calculation method.

--- src/XOMNI.CMS.FrontEnd/src/components/paginator/paginator.ts
+++ src/XOMNI.CMS.FrontEnd/src/components/paginator/paginator.ts
@@ -181,7 +181,7 @@ export class viewModel extends cms.infrastructure.baseViewModel {
             return this.range(1, totalPageCount);
         }
         else {
-            var frameIndex = currentPage % frameSize == 0 ? Math.floor(currentPage / frameSize) - 1 : Math.floor(currentPage / frameSize) ;
+            var frameIndex = currentPage % frameSize == 0 ? (currentPage / frameSize) - 1 : Math.floor(currentPage / frameSize) ;
             var lowerBound = frameIndex * frameSize + 1; 
             var upperBound = lowerBound + frameSize - 1 > totalPageCount ? totalPageCount : lowerBound + frameSize - 1;
             var currentFrame = this.range(lowerBound, upperBound);
